fe1bb8d899b87f98708cd36d277965d171ea643e
Zeloxus <kennmle@gmail.com>
2017-03-27 11:37:39 -0400

    Fuck me

--- Scripts/Highlighter.cs
+++ Scripts/Highlighter.cs
@@ -93,7 +93,7 @@ public class Highlighter : MonoBehaviour {
 				GameObject.FindGameObjectWithTag ("Character Display").GetComponent<SpriteRenderer>().sprite = selectedTile.containedCharacter.GetComponent<SpriteRenderer>().sprite;
 				GameObject.FindGameObjectWithTag ("Character Display").transform.localScale = new Vector3 (75, 75, 1);
 				GameObject.FindGameObjectWithTag ("Health Indicator").transform.localScale = new Vector3 (11.80754f, 71.82333f*selectedTile.containedCharacter.currentHP/selectedTile.containedCharacter.type.maximumHealth, 85.7285f);
-				GameObject.FindGameObjectWithTag ("Health Indicator").transform.localPosition = new Vector3 (369.7f,17f+72f*selectedTile.containedCharacter.currentHP/selectedTile.containedCharacter.type.maximumHealth,-102);
+				GameObject.FindGameObjectWithTag ("Health Label").transform.localPosition = new Vector3 (369.7f,17f+72f*selectedTile.containedCharacter.currentHP/selectedTile.containedCharacter.type.maximumHealth,-102);
 			}
 
 			this.executeSelectionModeActions ();
