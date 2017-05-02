fcb14230b660f8b833693f3e033bfa71a8baace9
Michael Andrzej Klaczynski <mak@rehost.com>
2017-05-02 03:33:25 -0400

    Continued work on Animation...

---
+++ Scripts/Animations/JumpMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMove : Animation {
+
+	// Use this for initialization
+	public override void Init () {
+		
+	}
+	
+	// Update is called once per frame
+	public override void Action () 
+	{
+		
+	}
+
+	public override bool IsFinished()
+	{
+		return false;
+	}
+
+	public override void OnFinish()
+	{
+
+	}
+}
---
+++ Scripts/Animations/Move.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+public class Move:Animation
+{
+	public Move (TileAttributes source, TileAttributes target, double speed)
+	{
+	}
+
+	public override void Init()
+	{
+
+	}
+
+	public override void Action()
+	{
+
+	}
+
+	public override bool IsFinished()
+	{
+		return false;
+	}
+
+	public override void OnFinish()
+	{
+
+	}
+}
